e44d5d47b815022e6ea1289fc95cf80442435ec7
Mobin Hor <m.hor@AZKI.CO>
2022-03-18 17:41:28 +0330

    creating showcase and aboutus:

--- cotainers/index/index.js
+++ cotainers/index/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
-
+import ShowCase from './showcase'
+import AboutUs from './aboutus'
 
 const HomePage = () => {
   return (
     <main>
-
+      <ShowCase />
+      <AboutUs />
     </main>
   )
 }
