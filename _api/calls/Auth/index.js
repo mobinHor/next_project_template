224dfad85c8bf6a6fbdffb9192c0d882ab7fe5ab
Mobin Hor <m.hor@AZKI.CO>
2022-03-27 14:19:35 +0430

    cleaning

--- _api/calls/Auth/index.js
+++ _api/calls/Auth/index.js
@@ -1,78 +1,14 @@
 import {api} from '../../index'
 
 
-export const GetCode = (email) => {
+// export const GetCode = (email) => {
     
-    const data = {
-        email : email
-    }
-    return api({
-        url : '/accounts/email_verification',
-        method : 'POST',
-        data : data
-    })
-}
-
-export const SignUp = (form) => {
-    
-    const data = {
-        email : form.email,
-        code : form.code,
-        password : form.password
-    }
-    return api({
-        url : '/accounts/sign_up',
-        method : 'POST',
-        data : data
-    })
-}
-
-export const Login = (form) => {
-    
-    const data = {
-        email : form.email,
-        password : form.password
-    }
-    return api({
-        url : '/accounts/login',
-        method : 'POST',
-        data : data
-    })
-}
-
-export const GetCodeForResetPass = (email) => {
-    
-    const data = {
-        email : email,
-    }
-    return api({
-        url : '/accounts/email_verification',
-        method : 'PATCH',
-        data : data
-    })
-}
-
-export const ResetPassword = (form) => {
-    
-    const data = {
-        email : form.email,
-        new_passowrd : form.password,
-        code : form.code
-    }
-
-    return api({
-        url : '/accounts/login',
-        method : 'PATCH',
-        data : data
-    })
-}
-
-export const GetUserInfo = () => {
-    
-    return api({
-        url : '/accounts/users',
-        method : 'POST',
-    } , 
-    true
-    )
-}
+//     const data = {
+//         email : email
+//     }
+//     return api({
+//         url : '/accounts/email_verification',
+//         method : 'POST',
+//         data : data
+//     })
+// }
